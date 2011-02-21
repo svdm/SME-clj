050c6d3c83c8d5187589e4b2e78e6ad6105e0eca
Stefan van der Meer <stefanvandermeer@gmail.com>
2011-02-21 18:08:02 +0100

    Added swank dep

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject sme-clj "1.0.0-SNAPSHOT"
   :description "Structure-Mapping Engine implementation in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
 
